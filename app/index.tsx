14e18b18807fc3312070dc1a6ec59ea43b756b8c
Patrick <1217303583@qq.com>
2024-08-25 22:22:52 +0800

    init

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+    return(
+        <SafeAreaView>
+            <Text>Home</Text>
+        </SafeAreaView>
+    )
+
+}
+
+export default Home;
